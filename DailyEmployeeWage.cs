d7419c7a59c08032908562bb69353809448220ec
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-10 22:50:17 +0530

    Calculate Employee Daily Wage Switch case using oops

--- DailyEmployeeWage.cs
+++ DailyEmployeeWage.cs
@@ -6,27 +6,27 @@ namespace EmployeeWageProblemUsingOops
 {
     class DailyEmployeeWage
     {
-        int EMP_RAT_PER_HOUR;
-        int IS_FULL_TIME;
-        int IS_PART_TIME;
-        public DailyEmployeeWage(int employeeRatePerHrs, int isFullTime,int isPartTime)
-        {
-            EMP_RAT_PER_HOUR = employeeRatePerHrs;
-            IS_FULL_TIME = isFullTime;
-            IS_PART_TIME = isPartTime;
-        }
+        const int EMP_RAT_PER_HOUR=20;
+        const int IS_FULL_TIME=1;
+        const int IS_PART_TIME=2;
         public int CalculateEmployeeDailyWage()
         {
             int empHour = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(2);
-            if (empCheck == IS_FULL_TIME)
-                empHour = 8;
-            else if (empCheck == IS_PART_TIME)
-                empHour = 4;
-            else
-                empHour = 0;
+            int empCheck = random.Next(3);
+            switch (empCheck)
+            {
+                case IS_FULL_TIME:
+                    empHour = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHour = 4;
+                    break;
+                default:
+                    empHour = 0;
+                    break;
+            }
             return empWage = empHour * EMP_RAT_PER_HOUR;
         }
     }
