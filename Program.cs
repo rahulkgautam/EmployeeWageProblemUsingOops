1f4035cb18153512a258d8b0787f1b1897f14d92
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-10 21:31:22 +0530

    Welcome to employee Wage problem using Oops concept

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWageProblemUsingOops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Wage Problem using oops");
+        }
+    }
+}
