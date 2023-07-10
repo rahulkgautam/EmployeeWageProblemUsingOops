31b40f748a34fb2da3872d6f3e6d7b0ca68fcfca
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-10 21:43:32 +0530

    Check employee attendance isPresent or not using oops

---
+++ EmployeeAttendance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageProblemUsingOops
+{
+    class EmployeeAttendance
+    {
+        public string CheckEmployeeAttendance()
+        {
+            int IS_PRESENT = 1;
+            Random random = new Random();
+            int empCheck = random.Next(2);
+            if (empCheck == IS_PRESENT)
+                return "Employee is Present";
+            else
+                return "Employee is Absent";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace EmployeeWageProblemUsingOops
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage Problem using oops");
+            var employeeAttendance = new EmployeeAttendance();
+            var isPresent =employeeAttendance.CheckEmployeeAttendance();
+            Console.WriteLine(isPresent);
         }
     }
 }
